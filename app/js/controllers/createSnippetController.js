6a45cca894ca08cdccc281ebf2d77f86d7c79cbc
Andrew S. Jones <contactandrewjones@gmail.com>
2014-05-01 23:29:03 -0400

    starting to flesh out the newSnippet page

--- app/js/controllers/createSnippetController.js
+++ app/js/controllers/createSnippetController.js
@@ -2,7 +2,12 @@
 
 mioDemoApp.controller('createSnippetController',
     function createSnippetController($scope) {
-        $scope.name = "";
-        $scope.code = "";
+        $scope.snippet = {};
+
+        $scope.saveSnippet = function(snippet, newSnippetForm) {
+            if (newSnippetForm.$valid) {
+
+            }
+        };
     }
 );
