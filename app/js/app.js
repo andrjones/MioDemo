b951d33b934e19df0c7bd743fdd13d96373a9752
Andrew S. Jones <contactandrewjones@gmail.com>
2014-05-01 22:17:06 -0400

    building out more structure

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,12 @@
 'use strict';
 
-var mioDemoApp = angular.module('mioDemoApp', []);
+var mioDemoApp = angular.module('mioDemoApp', ['ngRoute']);
+mioDemoApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.when('/newSnippet',
+        {
+            templateUrl: '/MioDemo/app/partials/newSnippet.html',
+            controller: 'createSnippetController'
+        });
+    $routeProvider.otherwise({redirectTo: '/newSnippet'});
+    $locationProvider.html5Mode(true);
+}]);
