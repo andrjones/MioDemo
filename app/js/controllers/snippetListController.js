02caea85d86421020cd875ee0a2043300ee88de0
Andrew S Jones <contactandrewjones@gmail.com>
2014-05-05 22:43:04 -0400

    added isolate scope and controller to snippet directive

--- app/js/controllers/snippetListController.js
+++ app/js/controllers/snippetListController.js
@@ -1,20 +1,7 @@
 'use strict';
 
 mioDemoApp.controller('snippetListController',
-    function snippetListController($scope, $location, snippetDataService, supportedLanguages) {
+    function snippetListController($scope, $location, snippetDataService) {
         $scope.snippets = snippetDataService.getAllSnippets();
-
-        $scope.loadSnippet = function(snippet) {
-            $location.url('/snippet/' + snippet.id);
-        };
-
-        $scope.getLanguageNameFromId = function(languageId) {
-            for (var i = 0; i < supportedLanguages.length; i++) {
-                if (supportedLanguages[i].id === languageId) {
-                    return supportedLanguages[i].name;
-                }
-            }
-            throw { name: "Lookup Error", message: "Unknown language Id"};
-        };
     }
 );
--- app/js/directives/snippetThumbnail.js
+++ app/js/directives/snippetThumbnail.js
@@ -4,6 +4,19 @@ mioDemoApp.directive('snippetThumbnail', function() {
     return {
         restrict: 'E',
         replace: true,
-        templateUrl: 'partials/directives/snippetThumbnail.html'
+        templateUrl: 'partials/directives/snippetThumbnail.html',
+        scope: {
+            snippet: '='
+        },
+        controller: function($scope, supportedLanguages) {
+            $scope.languageName = function(languageId) {
+                for (var i = 0; i < supportedLanguages.length; i++) {
+                    if (supportedLanguages[i].id === languageId) {
+                        return supportedLanguages[i].name;
+                    }
+                }
+                throw { name: "Lookup Error", message: "Unknown language Id"};
+            };
+        }
     }
 });
