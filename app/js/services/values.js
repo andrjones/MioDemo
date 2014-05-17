45d46c90a8d32657a18809cddacbd72bbd15d456
Andrew S Jones <contactandrewjones@gmail.com>
2014-05-17 09:00:09 -0400

    more unit testing

--- app/js/services/values.js
+++ app/js/services/values.js
@@ -7,3 +7,43 @@ angular.module('mioDemoApp').value("supportedLanguages", [
     { id: 3, name: 'Perl' },
     { id: 4, name: 'Scala' }
 ]);
+
+// this service is not used in the application, it is here for demonstrating
+// that services are interchangeable
+angular.module('mioDemoApp').factory("supportedLanguages2", function() {
+    return [
+        { id: 0, name: 'JavaScript' },
+        { id: 1, name: 'C#' },
+        { id: 2, name: 'Java' },
+        { id: 3, name: 'Perl' },
+        { id: 4, name: 'Scala' }
+    ];
+})
+
+// this service is not used in the application, it is here for demonstrating
+// testing simple (no dependencies) services
+angular.module('mioDemoApp').factory("supportedLanguages3", function() {
+    var languages = [
+        { id: 0, name: 'JavaScript' },
+        { id: 1, name: 'C#' },
+        { id: 2, name: 'Java' },
+        { id: 3, name: 'Perl' },
+        { id: 4, name: 'Scala' }
+    ];
+
+    var findLanguage = function(pattern) {
+        pattern = new RegExp(pattern);
+        var results = [];
+        for (var i = 0; i < languages.length; i++) {
+            if (pattern.test(languages[i].name)) {
+                results.push(languages[i]);
+            }
+        }
+        return results;
+    };
+
+    return {
+        languages: languages,
+        findLanguage: findLanguage
+    };
+});
---
+++ test/unit/valuesSpec.js
@@ -0,0 +1,36 @@
+'use strict';
+
+describe('values', function() {
+
+    // load the module that contains the controller under test
+    beforeEach(module('mioDemoApp'));
+
+    it('getLanguages should return an empty array when no langauge matches the pattern',
+        inject(function(supportedLanguages3) {
+            var pattern = "xyz";
+            var result = supportedLanguages3.findLanguage(pattern);
+            expect(result.length).toBe(0);
+    }));
+
+    it('getLanguages should return an array with one language when exactly one langauge matches the pattern',
+        inject(function(supportedLanguages3) {
+            var pattern = "^P.+";
+            var expected = [ {id: 3, name: 'Perl'} ];
+            var result = supportedLanguages3.findLanguage(pattern);
+            expect(result.length).toBe(1);
+            for(var i = 0; i < result.length; i++) {
+                expect(result[i]).toEqual(expected[i]);
+            }
+        }));
+
+    it('getLanguages should return an array of languages when multiple langauges matches the pattern',
+        inject(function(supportedLanguages3) {
+            var pattern = "^Ja.+";
+            var expected = [ {id: 0, name: 'JavaScript'}, {id: 2, name: 'Java'}];
+            var result = supportedLanguages3.findLanguage(pattern);
+            expect(result.length).toBe(2);
+            for(var i = 0; i < result.length; i++) {
+                expect(result[i]).toEqual(expected[i]);
+            }
+     }));
+});
